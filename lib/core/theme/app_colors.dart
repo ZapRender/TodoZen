e830d0649156e15e49ea340baa2e02e8d8af441e
Zamudio <alejandro.zamudio@onoff.com.co>
2025-02-24 01:29:56 -0500

    Feature: setup for theme app

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart' show Color;
+
+class AppColors {
+  //This is for light theme
+  static const primaryLight = Color(0xFF3F51B5);
+
+  //This is for dark theme
+  static const primaryDark = Color(0xFF8875FF);
+  static const secundaryDark = Color(0xFF3F51B5);
+  static const primaryTextDark = Color(0xFFFFFFFF);
+  static const secondaryTextDark = Color(0xFF979797);
+  static const darkBackground = Color(0xFF121212);
+  static const darksurface = Color(0xFF1E1E1E);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_zen/core/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark, //Change when light theme is ready
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
